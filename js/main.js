25a5d1d784524d0741497ac241c37ea57f4f1d3f
Thomas Davis <thomasalwyndavis@gmail.com>
2014-01-10 04:45:06 +1000

    social

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$.ajax('https://d28jjwuneuxo3n.cloudfront.net/?networks=facebook,twitter,googleplus&url=https://rally.stopwatching.us/', {
+            success: function (res, err) {
+              $.each(res, function(network, value){
+                var count = value;
+                if(count / 10000 > 1) {
+                  count = Math.ceil(count/1000) + 'k'
+                }
+                $('[data-network="'+network+'"]').attr('count', count);
+              })
+            },
+            dataType: 'jsonp'
+          })
