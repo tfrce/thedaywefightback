9492eed6dacc8e55a2947e36ecf204b432ea65b6
Thomas Davis <thomasalwyndavis@gmail.com>
2014-01-14 07:12:20 +1000

    use cloudfront

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ $.ajax('https://d28jjwuneuxo3n.cloudfront.net/?networks=facebook,twitter,googlep
 });
 // subscriber counts
 
-$.ajax('http://skipchimp2.herokuapp.com/count', {
+$.ajax('http://d1anv19wqyolnf.cloudfront.net/count', {
     success: function(res, err) {
         console.log(res);
         $('.subscribers-count').text('We have had ' + res.siteCount + ' websites sign up and ' + res.totalCount + ' individuals');
