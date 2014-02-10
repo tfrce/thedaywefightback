a67aeadec01ec2aae402a045ba90d1fb324d0892
Thomas Davis <thomasalwyndavis@gmail.com>
2014-02-11 00:58:56 +1000

    minus 3 hours to correct timezone

--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@ function splitTime(msec){
 // Update the day/hr/min/sec on page
 function updateTimeOnSite(timeDiffObj) {
 daysOd.update(timeDiffObj['d']);
-hoursOd.update(timeDiffObj['h']);
+hoursOd.update(timeDiffObj['h']-3);
 minutesOd.update(timeDiffObj['m']);
 secondsOd.update(timeDiffObj['s']);
 }
