908dbccdd1fc67bd2b97aa1921dfb928d27f4df5
Sina Khanifar <sina.khanifar@gmail.com>
2014-02-09 19:14:29 -0800

    day of action site

--- js/main.js
+++ js/main.js
@@ -349,7 +349,7 @@ if (!Object.keys) {
    ==========================================================================*/
 
 $('#video-preload').click( function(){
-    $('.video-container').html('<iframe width="853" height="480" src="//www.youtube-nocookie.com/embed/RJ194S7KjRg?rel=0&vq=hd1080&autoplay=1" frameborder="0" allowfullscreen></iframe>');
+    $('.video-container').html('<iframe width="853" height="480" src="//www.youtube-nocookie.com/embed/aGmiw_rrNxk?rel=0&vq=hd1080&autoplay=1" frameborder="0" allowfullscreen></iframe>');
 })
 
 
