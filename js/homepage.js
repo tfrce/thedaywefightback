13311f2f084e517ad61cb4422731483d24c394ec
sammularczyk <sam@samm.me>
2014-02-11 12:38:21 +1100

    added video section

--- js/homepage.js
+++ js/homepage.js
@@ -186,7 +186,10 @@ $(document).ready( function () {
     var src = $('.switch-locale').attr('href')
     $('.switch-locale').attr('href', src + '?greeting=' + greeting);
     
-    })
+    });
+    $('#video-preload').click( function(){
+        $('.video-container').addClass('playing').html('<iframe width="853" height="480" src="//www.youtube-nocookie.com/embed/aGmiw_rrNxk?rel=0&vq=hd1080&autoplay=1" frameborder="0" allowfullscreen></iframe>');
+    });
 });
 
 
